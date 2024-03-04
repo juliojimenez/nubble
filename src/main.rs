19fc0f6d352b5b7611396ea5a6a276b71abead93
Julio Jimenez <julio@julioj.com>
2024-03-04 00:08:40 -0500

    no idea
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use pnet::datalink::{self, Channel::Ethernet};
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use pnet::packet::Packet;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let interfaces = datalink::interfaces();
+    let interface_name = env::args().nth(1).unwrap();
+    let interface = interfaces.into_iter()
+        .find(|iface| iface.name == interface_name)
+        .expect("Failed to get interface.");
 }
